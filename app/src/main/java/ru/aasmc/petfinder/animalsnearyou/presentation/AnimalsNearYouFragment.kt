c2419914b51ef6359a390909739efb72822a3cf1
Alex Alexeyev <aasmc@yandex.ru>
2021-09-19 01:18:09 +0300

    Add some more skeleton classes and files

--- app/src/main/java/ru/aasmc/petfinder/animalsnearyou/presentation/AnimalsNearYouFragment.kt
+++ app/src/main/java/ru/aasmc/petfinder/animalsnearyou/presentation/AnimalsNearYouFragment.kt
@@ -1,6 +1,28 @@
 package ru.aasmc.petfinder.animalsnearyou.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.aasmc.petfinder.databinding.FragmentAnimalsNearYouBinding
 
 class AnimalsNearYouFragment: Fragment() {
+
+    private val binding get() = _binding!!
+
+    private var _binding: FragmentAnimalsNearYouBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAnimalsNearYouBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/ru/aasmc/petfinder/common/data/api/ApiConstants.kt
+++ app/src/main/java/ru/aasmc/petfinder/common/data/api/ApiConstants.kt
@@ -1,2 +1,27 @@
 package ru.aasmc.petfinder.common.data.api
 
+object ApiConstants {
+    const val BASE_ENDPOINT = "https://api.petfinder.com/v2/"
+    const val AUTH_ENDPOINT = "oauth2/token/"
+    const val ANIMALS_ENDPOINT = "animals"
+
+    const val KEY = "lM2hnfFg8kT0yxLRRC1J0Aze61YjnziU7iA0URrMK4yei1rHCb"
+    const val SECRET = "CoxnX1J1tlTJXhggiQbkyrTKlwc7u65vziwFSibA"
+}
+
+object ApiParameters {
+    const val TOKEN_TYPE = "Bearer "
+    const val AUTH_HEADER = "Authorization"
+    const val GRANT_TYPE_KEY = "grant_type"
+    const val GRANT_TYPE_VALUE = "client_credentials"
+    const val CLIENT_ID = "client_id"
+    const val CLIENT_SECRET = "client_secret"
+
+    const val PAGE = "page"
+    const val LIMIT = "limit"
+    const val LOCATION = "location"
+    const val DISTANCE = "distance"
+    const val NAME = "name"
+    const val AGE = "age"
+    const val TYPE = "type"
+}
