73a0b3ed5dd78ea22a4818a87283b65471f294d2
Alex Alexeyev <aasmc@yandex.ru>
2021-09-20 22:45:43 +0300

    Add interceptors to ApiModule

--- app/src/main/java/ru/aasmc/petfinder/common/data/di/ApiModule.kt
+++ app/src/main/java/ru/aasmc/petfinder/common/data/di/ApiModule.kt
@@ -10,13 +10,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.aasmc.petfinder.common.data.api.ApiConstants
 import ru.aasmc.petfinder.common.data.api.PetFinderApi
+import ru.aasmc.petfinder.common.data.api.interceptors.AuthenticationInterceptor
 import ru.aasmc.petfinder.common.data.api.interceptors.LoggingInterceptor
+import ru.aasmc.petfinder.common.data.api.interceptors.NetworkStatusInterceptor
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class ApiModule {
+object ApiModule {
 
+    /**
+     * [PetFinderApi] is a stateless interface that is used to make requests and
+     * return responses. So a [Singleton] is a perfect choice.
+     *
+     * Besides, each OkHttp instance has its own thread pool, which is expensive to create
+     * AND it also has a request cache on disk. So different OkHttp instances will have
+     * different caches. So [Singleton] is indeed a perfect choice.
+     */
     @Provides
     @Singleton
     fun provideApi(okHttpClient: OkHttpClient): PetFinderApi {
@@ -29,8 +39,18 @@ class ApiModule {
     }
 
     @Provides
-    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        networkStatusInterceptor: NetworkStatusInterceptor,
+        authenticationInterceptor: AuthenticationInterceptor
+    ): OkHttpClient {
+        // need to add interceptors in order:
         return OkHttpClient.Builder()
+            // first check network
+            .addInterceptor(networkStatusInterceptor)
+            // second check authentication
+            .addInterceptor(authenticationInterceptor)
+            // third add logging capabilities
             .addInterceptor(httpLoggingInterceptor)
             .build()
     }
